0d57be002656cf094ba920e840ffb1911288fdba
Dmitriy Burenok <dmitriyburenok@gmail.com>
2023-07-13 21:06:40 -0700

    Add script to build routes collection for easy search querying

---
+++ back-end/scripts/build_routes.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const MONGO_USER = process.env.BUSYBUS_MONGO_USER;
+const MONGO_PASS = process.env.BUSYBUS_MONGO_PASS;
+const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@busybuscluster0.eybkfr8.mongodb.net/?retryWrites=true&w=majority`;
+const routes = new Map();
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+(async () => {
+  const stopsCollection = client.db("busybus").collection("stops");
+  const stops = await stopsCollection.find({}).toArray();
+  const stopsWithRoutes = stops.filter((stop) => stop["Routes"] !== "");
+  for (const stop of stopsWithRoutes) {
+    const stopRoutes = stop["Routes"].split(", ");
+    stopRoutes.forEach((stopRoute) => {
+      if (!routes.has(stopRoute)) {
+        routes.set(stopRoute, [stop["StopNo"]]);
+      } else {
+        routes.set(stopRoute, [...routes.get(stopRoute), stop["StopNo"]]);
+      }
+    });
+  }
+
+  const routeObject = Object.fromEntries(routes);
+
+  const routesCollection = client.db("busybus").collection("routes");
+  await Promise.all(
+    Object.entries(routeObject).map(([route, busStopsArray]) =>
+      routesCollection.updateOne(
+        { _id: route },
+        { $set: { RouteNo: route, StopNos: busStopsArray } },
+        { upsert: true }
+      )
+    )
+  );
+
+  console.log(`DONE. Built ${routes.size} routes`);
+  process.exit(0);
+})();
